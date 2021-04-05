3233e0249d48f0cde6a0b8b523d323e67f228006
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-04-04 23:45:20 -0300

    inserção de dados na tabela tb_users atraves do pdo.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
         //echo '<p>conexao realizada com sucesso</p>';
 
         //criando tabela no db
+        /*
         $sql='create table tb_users(
             id int not null primary key auto_increment,
             nome varchar(50) not null,
@@ -20,6 +21,20 @@
             echo '<p>Tabela criada com sucesso.</p>';
         } else {
             echo '<p>falha ao criar tabela.</p>';
+        }
+        */
+
+        // inserindo registros no db
+
+        $sql='insert into tb_users( nome, email, senha) values("nique", "nique@teste.com.br", "654321")';
+
+        try {
+            $retorno = $conexao->exec($sql);
+            echo '<p>registro inserido com sucesso</p>';
+        } catch (PDOException $e) {
+            echo '<p>falha ao realizar registro</p>';
+            echo '<p>Erro:'.$e->getCode().'</p>';
+            echo '<p>Mensagem:'.$e->getMessage().'</p>';
         }
         
 
