a35736d1972a2ccfa10eb916e7593ec24efa3bb9
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-04-04 23:42:50 -0300

    criação de tabela usando a conexao criada.

--- index.php
+++ index.php
@@ -5,7 +5,23 @@
     $password = '';
     try {
         $conexao = new PDO('mysql:hostname='.$host.'dbname='.$db, $user, $password);
-        echo '<p>conexao realizada com sucesso</p>';
+        //echo '<p>conexao realizada com sucesso</p>';
+
+        //criando tabela no db
+        $sql='create table tb_users(
+            id int not null primary key auto_increment,
+            nome varchar(50) not null,
+            email varchar(100) not null,
+            senha varchar(32) not null
+        )';
+
+        $retorno = $conexao->exec($sql);
+        if (!$retorno) {
+            echo '<p>Tabela criada com sucesso.</p>';
+        } else {
+            echo '<p>falha ao criar tabela.</p>';
+        }
+        
 
     } catch (PDOException $e) {
         echo '<p>falha na conexão</p>';
